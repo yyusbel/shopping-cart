6a23f17eed24747d8b1e4dadc1847bf068499b53
Yusuf <yyusbel@yahoo.com>
2022-08-02 22:47:28 +0100

    4th commit

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
 import App from './App'
-import GlobalStyle from './theme/GlobalStyle'
+import GlobalStyle from './theme/GlobalStyles'
 import Theme from './theme/Theme'
 import { store } from './state/store/store'
 
